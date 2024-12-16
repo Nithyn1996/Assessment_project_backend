c2e81bd4f558dcaac8814e26b93d98c4848ed3c4
Nithyanantham <102945755+Nithyn1996@users.noreply.github.com>
2024-12-16 17:09:23 +0530

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,9 @@
 // /config/db.config.js
 module.exports = {
-    HOST: "b3a6cjp4aoybegvydkft-mysql.services.clever-cloud.com",
-    USER: "unc0bqktwmohlvuc",
-    PASSWORD: "OL8AzPT4GgjOZwHEAVKr",
-    DB: "b3a6cjp4aoybegvydkft",
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "root",
+    DB: "hello_crud",
     dialect: "mysql",
     pool: {
         max: 5,
